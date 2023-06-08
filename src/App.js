e58f48b9fbf84b75cc695f7a9d2ef8c0389f5c51
aasmah <aas.29980@gmail.com>
2023-06-08 16:38:20 -0500

    cart with restock function

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import './App.css';
+import  Products  from "./components/products";
+
+function App() {
+  return (
+    <Products></Products>
+  );
+}
+
+// export default function App() {
+//   return (
+//     <div className='App'>
+//       <Products/>
+//     </div>
+
+//   );
+// }
+
+export default App;
